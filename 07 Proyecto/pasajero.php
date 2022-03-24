7f6872064cb3ff642517583d7eac5e2bcf947ebe
Edinsonn <edinson.hormiga6@misena.edu.com>
2022-03-24 08:11:33 -0500

    ten commit

--- 07 Proyecto/pasajero.php
+++ 07 Proyecto/pasajero.php
@@ -4,6 +4,11 @@ require_once("Templates/template_formulario.php");
 
 <div class="formulario"
 <form action="" method="">
+<div class="text-center mb-4">
+        <img class="mb-4" src="imagen/logo.png" alt="" width="72" height="72">
+        <h1 class="h3 mb-3 font-weight-normal">Registrarme</h1>
+        
+      </div>
   <div class="mb-3">
     <label>Nombres Completos</label>
     <input type="text" name="nombre" class="form-control">
